e440499ab3313e3cfd39e7a22997ccfb2f286813
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-12-15 23:40:30 +0900

    [FEAT] localforage를 이용한 IndexdDB 사용

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,19 @@
+import localforage from 'localforage';
 import { rest } from 'msw'
+import { LoginPayload } from './types/user';
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    const { username } = req.body
+  rest.post('/login', async (req, res, ctx) => {
+    const user: LoginPayload = await req.json();
+    
+    localforage.setItem('test', {
+      'name': user.username,
+    })
 
     return res(
       ctx.json({
         id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
-        username,
+        username: user.username,
         firstName: 'John',
         lastName: 'Maverick',
       })
